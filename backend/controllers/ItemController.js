8b7568c92ab58bac08b9adf1ec7da890aab7b728
ABEL-1010 <abeldemoz4@gmail.com>
2026-01-12 19:43:51 +0300

    itemController

--- backend/controllers/ItemController.js
+++ backend/controllers/ItemController.js
@@ -1,5 +1,6 @@
 import Item from "../models/Item";
 import asyncHandler from "express-async-handler";
+import Sale from "../models/Sale";
 
 export const getItem = asyncHandler(async(req, res) => {
     const item = await Item.findById(req.params.id);
@@ -129,3 +130,57 @@ export const updateItem = asyncHandler( async( req, res) => {
         throw new Error('Item not found');
     }
 });
+
+export const deleteItem = asyncHandler( async(req, res) => {
+     
+    //find item by from the request parameter
+    const item = await Item.findById(req.params.id);
+    //check if the item is in the database
+    if(item) {
+        //if exist,  check if there are any sale record
+        //ab wushti Ssle collection where item fiesld matches items Id
+        const salesWithItem = await Sale.find( { item: item._id});
+            if( salesWithItem.length>0){
+                //deletes all sales records associated eith the item
+                await Sale.deleteMany({ item: item._id});
+            }
+            //delete the Item itself from the db
+        await Item.deleteOne( { _id: item._id});
+        //send success report to client
+        res.json({ message: 'Item and Sale deleted'})
+    }else {
+        res.status(404);
+        throw new Error( ' item not found');
+    }
+
+})
+export const getItemsByCategory = asyncHandler( async( req, res) => {
+    //ab route nserhela ena 
+    ///categories/:id/items
+    const itemsWithCategory = await Item.find({ category: req.params.categoryId})
+    //populate the category with specific data from the category collection
+    .populate('category', 'name description');
+
+    res.status(itemsWithCategory)
+})
+
+export const getItems = asyncHandler( async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const itemLimit = parseInt(req.query.search) || 10;
+    const search = req.query.search || '';
+    const category = req.query.category || '';
+    const sortBy = req.query.sortBy || 'createdAt';
+    const sortOrder = req.query.sortOrder === 'asc'? 1 : -1;
+    const skip = (page - 1) * itemLimit;
+
+    const filter = {};
+    if(search){
+        filter.$or = [
+            {name : { $regex: search, $option: 'i'}},
+            {description: { $regex: search, $optionoptions: 'i'}}
+        ];
+    }
+    if(category){
+        filter.category = category;
+    }
+})
